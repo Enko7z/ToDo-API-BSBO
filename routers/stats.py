c5195b21636e033c141e0b2f6c7ae4960642869f
Иван Иванов <Ivan@mail.com>
2025-12-09 22:07:29 +0300

    5 часть практики настройка подключение к PostgreSQL через SQLAlchemy до самостоятельного задания с дедлайном

--- routers/stats.py
+++ routers/stats.py
@@ -1,46 +1,31 @@
-from fastapi import APIRouter
-from database import tasks_db
-from typing import Dict, Any
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from models import Task
+from database import get_async_session
+
 
 router = APIRouter(
     prefix="/stats",
-    tags=["stats"],
-    responses={404: {"description": "Stats not found"}},
+    tags=["statistics"]
 )
 
-"""
-2) создайте endpoint для получения статистики по задачам:
-GET /tasks/stats
-async def get_tasks_stats() -> dict:
-Возвращает:
-- Общее количество задач
-- Количество задач в каждом квадранте (Q1, Q2, Q3, Q4)
-- Количество выполненных и невыполненных задач.
-Ожидаемый формат ответа: { "total_tasks": 5, "by_quadrant": { "Q1": 1,
-"Q2": 1, "Q3": 1, "Q4": 1 }, "by_status": { "completed": 1, "pending": 3 } }
-"""
-@router.get("/stats")
-async def get_tasks_stats() -> dict:
-    # Общее количество задач
-    total_tasks = len(tasks_db)
-    
-    # Количество задач по квадрантам
-    by_quadrant = {
-        "Q1": len([task for task in tasks_db if task["quadrant"] == "Q1"]),
-        "Q2": len([task for task in tasks_db if task["quadrant"] == "Q2"]),
-        "Q3": len([task for task in tasks_db if task["quadrant"] == "Q3"]),
-        "Q4": len([task for task in tasks_db if task["quadrant"] == "Q4"])
-    }
-    
-    # Количество задач по статусу выполнения
-    completed_count = len([task for task in tasks_db if task["completed"]])
-    pending_count = len([task for task in tasks_db if not task["completed"]])
-    
-    by_status = {
-        "completed": completed_count,
-        "pending": pending_count
-    }
-    
+@router.get("/", response_model=dict)
+async def get_tasks_stats(db: AsyncSession = Depends(get_async_session)) -> dict:
+    result = await db.execute(select(Task))
+    tasks = result.scalars().all()
+    total_tasks = len(tasks)
+    by_quadrant = {"Q1": 0, "Q2": 0, "Q3": 0, "Q4": 0}
+    by_status = {"completed": 0, "pending": 0}
+
+    for task in tasks:
+        if task.quadrant in by_quadrant:
+            by_quadrant[task.quadrant] += 1
+        if task.completed:
+            by_status["completed"] += 1
+        else:
+            by_status["pending"] += 1
+
     return {
         "total_tasks": total_tasks,
         "by_quadrant": by_quadrant,
